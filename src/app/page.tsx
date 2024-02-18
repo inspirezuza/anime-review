0ec0f3758f32a6ba556e294c5ac98b5e3ab7c89a
Patthadon Tantiameorn <beebobbob956@gmail.com>
2024-02-19 00:06:41 +0700

    filtering page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,81 @@
-import Image from "next/image";
+"use client";
+import { Tabs, Tab, Switch } from "@nextui-org/react";
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+import Card from "./component/card";
 
 export default function Home() {
+  const [day, setDay] = useState<string>("monday");
+  const [anime, setAnime] = useState<any>([]);
+  const [nsfw, setNSFW] = useState<boolean>(false);
+
+  const fetchAnime = async () => {
+    const supabase = createClient(
+      "https://ljalvqncjekbwpdnzaby.supabase.co",
+      process.env.NEXT_PUBLIC_SUPABASE_KEY as string
+    );
+    if (nsfw) {
+      let { data: anime, error } = await supabase
+        .from("anime")
+        .select("*")
+        .like("broadcastday", day);
+      setAnime(anime);
+    } else {
+      let { data: anime, error } = await supabase
+        .from("anime")
+        .select("*")
+        .like("broadcastday", day)
+        .eq("SFW", true);
+      setAnime(anime);
+    }
+    console.log(anime);
+  };
+
+  useEffect(() => {
+    fetchAnime();
+  }, [day, nsfw]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="w-screen h-screen overflow-hidden bg-black">
+      <div className="w-screen my-1 px-2 flex justify-end">
+        <Switch isSelected={nsfw} onValueChange={setNSFW} color="danger">
+          <p className="text-primary">NSFW</p>
+        </Switch>
+      </div>
+      <div className="flex justify-center">
+        <div className="w-[22rem]">
+          <div className="flex flex-wrap gap-7 w-full max-w-screen overflow-auto my-[1rem]">
+            <Tabs
+              selectedKey={day}
+              onSelectionChange={(key) => setDay(key as string)}
+              aria-label="Tabs colors"
+              variant="solid"
+              radius="none"
+            >
+              <Tab key="%" title="All" />
+              <Tab key="monday" title="Monday" />
+              <Tab key="tuesday" title="Tuesday" />
+              <Tab key="wednesday" title="Wednesday" />
+              <Tab key="thursday" title="Thursday" />
+              <Tab key="friday" title="Friday" />
+              <Tab key="saturday" title="Saturday" />
+              <Tab key="sunday" title="Sunday" />
+            </Tabs>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="h-full overflow-auto">
+        {anime.map((a: any, index: any) =>
+          index % 2 != 0 ? null : (
+            <div className="flex w-screen justify-evenly">
+              <Card key={a.id} anime={a}></Card>
+              {anime[index + 1] ? (
+                <Card key={anime[index + 1].id} anime={anime[index + 1]}></Card>
+              ) : null}
+            </div>
+          )
+        )}
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,15 @@
 import type { Config } from "tailwindcss";
 
+const { nextui } = require("@nextui-org/react");
+
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/flowbite/**/*.js",
+    "./node_modules/flowbite-react/lib/**/*.js",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -15,6 +20,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [require("flowbite/plugin"), nextui()],
 };
 export default config;
