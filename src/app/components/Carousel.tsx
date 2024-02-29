48fd32c622eab4eaa9cd4287b741df01f21b3175
Patthadon Tantiameorn <beebobbob956@gmail.com>
2024-02-29 22:53:26 +0700

    add homepage

---
+++ src/app/components/Carousel.tsx
@@ -0,0 +1,86 @@
+import { Carousel as Carouselflow } from "flowbite-react";
+import { useEffect, useState } from "react";
+import { createClient } from "@/utils/supabase/client";
+import Image from "next/image";
+
+const supabase = createClient();
+
+export default function Carousel() {
+  const [anime, setAnime] = useState<any>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const fetchAnime = async () => {
+    setLoading(true);
+    let { data: anime, error } = await supabase
+      .from("anime")
+      .select("*,anime-genre(anime_id,Genre_id),genre(id,genrename)")
+      .eq("SFW", true)
+      .order("rating", { ascending: false })
+      .range(0, 5);
+    setAnime(anime);
+    setLoading(false);
+    console.log(anime);
+  };
+  useEffect(() => {
+    fetchAnime();
+  }, []);
+  return (
+    <div>
+      <div className="h-[15rem] w-screen">
+        {loading ? (
+          <Carouselflow>
+            <img
+              src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
+              alt="..."
+            />
+          </Carouselflow>
+        ) : (
+          <Carouselflow>
+            {anime.map((anime: any) => (
+              <div key={anime.id}>
+                <Image
+                  src={anime.main_picture}
+                  width={100}
+                  height={50}
+                  alt={anime.title}
+                  className=" object-cover w-full h-full"
+                ></Image>
+              </div>
+            ))}
+          </Carouselflow>
+        )}
+      </div>
+      <div className="w-screen">
+        <div>Top Rating</div>
+        {!loading &&
+          anime.map((anime: any, index: number) => (
+            <div className="grid grid-cols-2 mx-2 my-2 items-center">
+              <div>
+                <div>
+                  {index + 1}.{anime.title}
+                </div>
+                <div>Rating : {anime.rating}</div>
+                <div className="flex text-gray-600 gap-2">
+                  {anime.genre.map((genre: any) => (
+                    <div key={genre.id} className="flex">
+                      {`${genre.genrename}`}
+                    </div>
+                  ))}
+                </div>
+              </div>
+              <div className="w-full flex justify-center">
+                <Image
+                  src={anime.main_picture}
+                  width={100}
+                  height={50}
+                  alt={anime.title}
+                  className=" object-cover"
+                ></Image>
+              </div>
+            </div>
+          ))}
+        <div className="h-[5rem]"></div>
+      </div>
+    </div>
+  );
+}
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -13,29 +13,124 @@ import React from "react";
 
 export default function Bottomnavbar() {
   return (
-    <div className="w-screen flex justify-center fixed bottom-0">
-      <div className="">
-        <Navbar isBordered classNames={{ item: ["flex", "rounded-sm"] }}>
-          <NavbarContent>
-            <NavbarContent justify="center" className="gap-4 flex">
-              <NavbarItem>
-                <Link color="foreground" href="/">
-                  <IoMdHome />
-                </Link>
-              </NavbarItem>
-              <NavbarItem>
-                <Link color="foreground" href="/">
-                  <IoMdSearch />
-                </Link>
-              </NavbarItem>
-              <NavbarItem>
-                <Link color="foreground" href="/login">
-                  <FaUser size={14} />
-                </Link>
-              </NavbarItem>
-            </NavbarContent>
-          </NavbarContent>
-        </Navbar>
+    <div className="fixed z-50 w-[20rem] h-[3rem] max-w-lg -translate-x-1/2 bg-white border border-gray-200 rounded-full bottom-4 left-1/2 dark:bg-gray-700 dark:border-gray-600">
+      <div className="grid h-full max-w-lg grid-cols-4 mx-auto">
+        <Link href="\" className="w-full h-full">
+          <button
+            data-tooltip-target="tooltip-home"
+            type="button"
+            className="inline-flex flex-col items-center justify-center px-5 rounded-s-full hover:bg-gray-50 dark:hover:bg-gray-800 w-full  h-full"
+          >
+            <svg
+              className="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
+            </svg>
+            <span className="sr-only">Home</span>
+          </button>
+
+          <div
+            id="tooltip-home"
+            role="tooltip"
+            className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+          >
+            Home
+            <div className="tooltip-arrow" data-popper-arrow></div>
+          </div>
+        </Link>
+        <Link href="/search">
+          <button
+            data-tooltip-target="tooltip-wallet"
+            type="button"
+            className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group h-full w-full"
+          >
+            <svg
+              className="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-width="3"
+                d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"
+              />
+            </svg>
+            <span className="sr-only">Wallet</span>
+          </button>
+        </Link>
+        <div
+          id="tooltip-wallet"
+          role="tooltip"
+          className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+        >
+          Wallet
+          <div className="tooltip-arrow" data-popper-arrow></div>
+        </div>
+        <Link href="\">
+          <button
+            data-tooltip-target="tooltip-settings"
+            type="button"
+            className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group h-full w-full"
+          >
+            <svg
+              className="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2.1"
+                d="m17 21-5-4-5 4V3.9c0-.2 0-.5.2-.6l.6-.3h8.4c.2 0 .4 0 .6.3l.2.6V21Z"
+              />
+            </svg>
+            <span className="sr-only">Settings</span>
+          </button>
+        </Link>
+        <div
+          id="tooltip-settings"
+          role="tooltip"
+          className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+        >
+          Settings
+          <div className="tooltip-arrow" data-popper-arrow></div>
+        </div>
+        <Link href="\login">
+          <button
+            data-tooltip-target="tooltip-profile"
+            type="button"
+            className="inline-flex flex-col items-center justify-center px-5 rounded-e-full hover:bg-gray-50 dark:hover:bg-gray-800 group h-full w-full"
+          >
+            <svg
+              className="w-5 h-5 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
+            </svg>
+            <span className="sr-only">Profile</span>
+          </button>
+        </Link>
+        <div
+          id="tooltip-profile"
+          role="tooltip"
+          className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+        >
+          Profile
+          <div className="tooltip-arrow" data-popper-arrow></div>
+        </div>
       </div>
     </div>
   );
