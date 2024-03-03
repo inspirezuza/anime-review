3eb3e7723d04362d66783f0af3e56173e9cd810d
inspirezuza <inspire7220@gmail.com>
2024-03-04 04:57:45 +0700

    add login to comment

--- src/components/component/NewComment.tsx
+++ src/components/component/NewComment.tsx
@@ -3,16 +3,22 @@ import { cookies } from "next/headers";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import { revalidatePath } from "next/cache";
+import Link from "next/link";
 
-export default function NewComment({ anime_id }: { anime_id: string }) {
+export default async function NewComment({ anime_id }: { anime_id: string }) {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
   const addTweet = async (formData: FormData) => {
     "use server";
-    const title = String(formData.get("title"));
     const cookieStore = cookies();
     const supabase = createClient(cookieStore);
     const {
       data: { user },
     } = await supabase.auth.getUser();
+    const title = String(formData.get("title"));
     if (user) {
       const { data, error } = await supabase
         .from("comments")
@@ -25,12 +31,22 @@ export default function NewComment({ anime_id }: { anime_id: string }) {
   };
 
   return (
-    <form
-      className="flex justify-center w-full max-w-md items-center space-x-2"
-      action={addTweet}
-    >
-      <Input name="title" placeholder="Add new comment!" />
-      <Button type="submit">submit</Button>
-    </form>
+    <div>
+      {user ? (
+        <form
+          className="flex justify-center w-full max-w-md items-center space-x-2"
+          action={addTweet}
+        >
+          <Input name="title" placeholder="Add new comment!" />
+          <Button type="submit">submit</Button>
+        </form>
+      ) : (
+        <Link className="" href={"/login"}>
+          <Button className="w-full max-w-md mx-auto p-4">
+            login to comment
+          </Button>
+        </Link>
+      )}
+    </div>
   );
 }
